985f23307a02208d32dd9ef3e41484e880657ae5
Gustavo6401 <gsoey2019123@gmail.com>
2023-11-03 23:31:34 -0300

    Criação de DbContext

---
+++ InfraEstrutura/Contexto/AnimeContext.cs
@@ -0,0 +1,33 @@
+﻿using Dominio.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestrutura.Contexto
+{
+    // Essa Classe só será acessada pela camada "Repositórios"
+    internal class AnimeContext : DbContext
+    {
+        public AnimeContext(DbContextOptions<AnimeContext> options) : base(options) { }
+        public DbSet<Anime> Animes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Anime>().HasKey(a => a.Id);
+
+            modelBuilder.Entity<Anime>()
+                .Property(a => a.Nome)
+                    .HasMaxLength(100)
+                        .IsRequired();
+
+            modelBuilder.Entity<Anime>()
+                .Property(a => a.NumeroEpisodios)
+                    .IsRequired();
+
+            modelBuilder.Entity<Anime>()
+                .Property(a => a.Status)
+                    .HasMaxLength(20)
+                        .IsRequired();
+        }
+
+        public virtual void Modify(object entity) => Entry(entity).State = EntityState.Modified;
+    }
+}
