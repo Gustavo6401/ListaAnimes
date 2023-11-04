35aa5c83d87e12023afdb4b2cbd8a26c0f7d0584
Gustavo6401 <gsoey2019123@gmail.com>
2023-11-03 23:09:03 -0300

    Criadas as Classes Repository e a classe da tabela.

--- Dominio/Interfaces/Repositories/Base/IRepository.cs
+++ Dominio/Interfaces/Repositories/Base/IRepository.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Dominio.Interfaces.Repositories.Base
 {
-    internal interface IRepository
+    public interface IRepository <T> where T : class
     {
+        public Task<IEnumerable<T>> GetAllAsync();
+        public Task<T> GetByIdAsync(int id);
+        public Task CreateAsync(T entity);
+        public Task UpdateAsync(T entity);
+        public Task DeleteAsync(int id);
     }
 }
