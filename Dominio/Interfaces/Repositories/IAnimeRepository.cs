5522325f3ab144787d043cdf9f982917a931c5bb
Gustavo6401 <gsoey2019123@gmail.com>
2023-11-03 23:16:05 -0300

    Interfaces da Camada Services concluídas.

---
+++ Dominio/Interfaces/Repositories/IAnimeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Dominio.Entities;
+using Dominio.Interfaces.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Interfaces.Repositories
+{
+    public interface IAnimeRepository : IRepository<Anime>
+    {
+    }
+}
---
+++ Dominio/Interfaces/Services/Base/IServiceBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Interfaces.Services.Base
+{
+    public interface IServiceBase<T, U> where T : class where U : class
+    {
+        public Task<IEnumerable<T>> GetAllAsync();
+        public Task<T> GetByIdAsync(int id);
+        public Task CreateAsync(T entity);
+        public Task UpdateAsync(T entity);
+        public Task DeleteAsync(int id);
+    }
+}
